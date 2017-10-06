f714775cae1e86d0fdef52c302d9a96405d578b2
orthagonal <orthagonal1@gmail.com>
2017-10-06 18:48:12 -0500

    store 1

--- plugins/store.js
+++ plugins/store.js
@@ -1,11 +1,13 @@
-const ioredis = require('ioredis');
+const Redis = require('ioredis');
 
 exports.register = function(server, options, next) {
   let cache = server.settings.app.cache;
-  if (options.redis.host) {
-    cache = '';
+  if (server.settings.app.redis.host) {
+    cache = new Redis(server.settings.app.redis);
   }
-  // server.decorate('server', 'store', (method, url, payload, done) => {
+  server.decorate('server', 'get', cache.get);
+  server.decorate('server', 'set', cache.set);
+};
 
 exports.register.attributes = {
   name: 'store'
