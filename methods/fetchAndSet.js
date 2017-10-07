87b006806f28994e13f8b70d29d445438926db93
orthagonal <orthagonal1@gmail.com>
2017-10-07 00:21:38 -0500

    get path and set key to result

--- methods/fetchAndSet.js
+++ methods/fetchAndSet.js
@@ -2,8 +2,7 @@ const wreck = require('wreck');
 
 module.exports = function(path, key, allDone) {
   const server = this;
-  const cacheKey = `${this.settings.app.origin}${path}`;
-  wreck.get(cacheKey, (err, response) => {
+  wreck.get(path, (err, response) => {
     if (err) {
       return allDone(err);
     }
