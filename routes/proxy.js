1bcc0d4e92119204a36130b33d5455a777f2ac2a
orthagonal <orthagonal1@gmail.com>
2017-10-07 21:57:26 -0500

    proxy and test 1

--- routes/proxy.js
+++ routes/proxy.js
@@ -1,18 +1,26 @@
-module.exports = {
+module.exports.proxy = {
   method: 'get',
   path: '/{path*}',
-  handler: function(request, reply) {
-    const requestedUrl = request.url;
-    const cacheKey = server.getCacheKey(requestedUrl);
-    this.get(cacheKey, (err, cachedValue) => {
+  handler(request, reply) {
+    const server = request.server;
+    const requestedUrl = request.url.path;
+    const cacheKey = server.methods.getCacheKey(requestedUrl);
+    server.store.get(cacheKey, (err, cachedValue) => {
       if (err) {
         return reply(err);
       }
       if (!cachedValue) {
-
+        server.log(['micro-cache'], `key ${cacheKey} cache hit`);
+        return reply(null, cachedValue);
       }
-      this.log(['micro-cache', `key ${cacheKey} cache hit`]);
-      return reply(null, cachedValue);
+      server.methods.fetchAndSet(cacheKey, (originErr, originValue) => {
+        if (originErr) {
+          return reply(originErr);
+        }
+        server.log(['micro-cache'], `key ${cacheKey} cache miss`);
+        server.store.set(cacheKey, originValue);
+        return reply(null, originValue);
+      });
     });
   }
 };
