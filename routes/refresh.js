0a0f39cbf8e3a7e06084d0ac2a7b69bc56bfbfad
orthagonal <orthagonal1@gmail.com>
2017-10-08 21:16:14 -0500

    Route refresh (#10)
    * routeFresh
    
    * refresh

---
+++ routes/refresh.js
@@ -0,0 +1,41 @@
+const joi = require('joi');
+const boom = require('boom');
+const async = require('async');
+
+module.exports.refresh = {
+  method: 'POST',
+  path: '/_refresh',
+  config: {
+    validate: {
+      payload: {
+        secret: joi.string(),
+        urls: joi.array().items(joi.string())
+      }
+    }
+  },
+  handler(request, reply) {
+    const server = request.server;
+    if (request.payload.secret !== server.settings.app.secret) {
+      return reply(boom.unauthorized);
+    }
+    // reply without delay:
+    reply(null, { accepted: true });
+    // refresh each method:
+    async.each(request.payload.urls, (url, eachDone) => {
+      server.methods.getKeys(url, (err, urlList) => {
+        if (err) {
+          server.log(err);
+        }
+        urlList.forEach((path) => {
+          const key = server.methods.getCacheKey(path);
+          server.methods.fetchAndSet(path, key, (fetchErr, result) => {
+            if (fetchErr) {
+              server.log(fetchErr);
+            }
+          });
+        });
+        eachDone();
+      });
+    });
+  }
+};
