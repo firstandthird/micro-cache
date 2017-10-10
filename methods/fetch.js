230daeb309112f8efecaeff64853eba7b9e94f88
orthagonal <orthagonal1@gmail.com>
2017-10-09 21:08:35 -0500

    handle async err internally

--- methods/fetch.js
+++ methods/fetch.js
@@ -2,14 +2,12 @@ const wreck = require('wreck');
 
 module.exports = function(method, path, allDone) {
   const cacheKey = `${this.settings.app.origin}${path}`;
-  const callWreck = async () => {
-    const { res, payload } = await wreck[method.toLowerCase()](cacheKey);
-    return allDone(null, JSON.parse(payload.toString()));
+  async function callWreck() {
+    try {
+      const { res, payload } = await wreck[method.toLowerCase()](cacheKey);
+      return allDone(null, JSON.parse(payload.toString()));
+    } catch (e) {
+      return allDone(err);
+    }
   };
-  try {
-    callWreck();
-  }
-  catch (err) {
-    return allDone(err);
-  }
 };
