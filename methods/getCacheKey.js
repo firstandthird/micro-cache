1afc370cdcfbd627033bbf1c2746656fa042c4fc
Chris <orthagonal1@gmail.com>
2017-10-11 16:48:43 -0500

    remove query-parse, use map/join

--- methods/getCacheKey.js
+++ methods/getCacheKey.js
@@ -2,16 +2,7 @@ module.exports = function(path, query) {
   if (!query) {
     return `${this.settings.app.redis.prefix}-${path}`;
   }
-  let querystring = '';
   const keys = Object.keys(query).sort();
-  let first = true;
-  keys.forEach((key) => {
-    if (first) {
-      querystring = `?${key}=${query[key]}`;
-      first = false;
-      return;
-    }
-    querystring = `${querystring}&${key}=${query[key]}`;
-  });
-  return `${this.settings.app.redis.prefix}-${path}${querystring}`;
+  const querystring = keys.map((key) => `${key}=${query[key]}`);
+  return `${this.settings.app.redis.prefix}-${path}?${querystring.join('&')}`;
 };
