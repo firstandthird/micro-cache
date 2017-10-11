752bfe35168cc9a2e6e5501f1e4c27d5f5f5d4ac
orthagonal <orthagonal1@gmail.com>
2017-10-10 21:54:38 -0500

    log object (#15)

--- routes/proxy.js
+++ routes/proxy.js
@@ -10,14 +10,14 @@ module.exports.proxy = {
         return reply(err);
       }
       if (cachedValue) {
-        server.log(['micro-cache'], `key ${cacheKey} cache hit`);
+        server.log(['micro-cache'], { key: cacheKey, cache: 'hit' });
         return reply(null, cachedValue);
       }
       server.methods.fetchAndSet(request.url.path, cacheKey, (originErr, originValue) => {
         if (originErr) {
           return reply(originErr);
         }
-        server.log(['micro-cache'], `key ${cacheKey} cache miss`);
+        server.log(['micro-cache'], { key: cacheKey, cache: 'miss' });
         server.store.set(cacheKey, originValue);
         return reply(null, originValue);
       });
